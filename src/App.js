82ee83cdbb0bad22ced6bac59f362189464a2433
Gene Jeoung <jeounggene@gmail.com>
2021-07-08 00:37:39 -0700

    scroll to top

--- src/App.js
+++ src/App.js
@@ -10,21 +10,25 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/pages/Footer.js/Footer";
 import Terms from "./components/pages/PPnTOS/Terms";
 import Policy from "./components/pages/PPnTOS/Policy";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/services" component={Services} />
-        <Route path="/products" component={AboutUs} />
-        <Route path="/sign-up" component={Download} />
-        <Route path="/terms" component={Terms} />
-        <Route path="/policy" component={Policy} />
+      <ScrollToTop>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/services" component={Services} />
+          <Route path="/products" component={AboutUs} />
+          <Route path="/sign-up" component={Download} />
+          <Route path="/terms" component={Terms} />
+          <Route path="/policy" component={Policy} />
 
-        {/* <Route path='/download' component={Download} /> */}
-      </Switch>
+          {/* <Route path='/download' component={Download} /> */}
+        </Switch>
+      </ScrollToTop>
+
       <Footer />
     </Router>
   );
--- src/components/pages/Footer.js/Footer.js
+++ src/components/pages/Footer.js/Footer.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import './Footer.css';
-import { Button } from '../../Button';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Footer.css";
+import { Button } from "../../Button";
+import { Link } from "react-router-dom";
 import {
   FaFacebook,
   FaInstagram,
   FaYoutube,
   FaTwitter,
   FaLinkedin,
-  FaPencilAlt
-} from 'react-icons/fa';
+  FaPencilAlt,
+} from "react-icons/fa";
 
 function Footer() {
   return (
-    <div className='footer-container'>
+    <div className="footer-container">
       {/* <section className='footer-subscription'>
         <p className='footer-subscription-heading'>
           Join our exclusive membership to receive the latest news and trends
@@ -33,90 +33,90 @@ function Footer() {
           </form>
         </div>
       </section> */}
-      <div className='footer-links'>
-        <div className='footer-link-wrapper'>
-          <div className='footer-link-items'>
+      <div className="footer-links">
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
             <h2>About Us</h2>
-            <Link to='/sign-up'>How it works</Link>
-            <Link to='/'>Testimonials</Link>
-            <Link to='/'>Careers</Link>
-            <Link to='/'>Investors</Link>
-            <Link to='/'>Terms of Service</Link>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of Service</Link>
           </div>
-          <div className='footer-link-items'>
+          <div className="footer-link-items">
             <h2>Contact Us</h2>
-            <Link to='/'>Contact</Link>
-            <Link to='/'>Support</Link>
-            <Link to='/'>Destinations</Link>
-            <Link to='/'>Sponsorships</Link>
+            <Link to="/">Contact</Link>
+            <Link to="/">Support</Link>
+            <Link to="/">Destinations</Link>
+            <Link to="/">Sponsorships</Link>
           </div>
         </div>
-        <div className='footer-link-wrapper'>
-          <div className='footer-link-items'>
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
             <h2>Videos</h2>
-            <Link to='/'>Submit Video</Link>
-            <Link to='/'>Ambassadors</Link>
-            <Link to='/'>Agency</Link>
-            <Link to='/'>Influencer</Link>
+            <Link to="/">Submit Video</Link>
+            <Link to="/">Ambassadors</Link>
+            <Link to="/">Agency</Link>
+            <Link to="/">Influencer</Link>
           </div>
-          <div className='footer-link-items'>
+          <div className="footer-link-items">
             <h2>Social Media</h2>
-            <Link to='/'>Instagram</Link>
-            <Link to='/'>Facebook</Link>
-            <Link to='/'>Youtube</Link>
-            <Link to='/'>Twitter</Link>
+            <Link to="/">Instagram</Link>
+            <Link to="/">Facebook</Link>
+            <Link to="/">Youtube</Link>
+            <Link to="/">Twitter</Link>
           </div>
         </div>
       </div>
-      <section className='social-media'>
-        <div className='social-media-wrap'>
-          <div className='footer-logo'>
-            <Link to='/' className='social-logo'>
-              <FaPencilAlt className='navbar-icon' />
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div className="footer-logo">
+            <Link to="/" className="social-logo">
+              <FaPencilAlt className="navbar-icon" />
               Counsly
             </Link>
           </div>
-          <small className='website-rights'>Counsly © 2021</small>
-          <div className='social-icons'>
+          <small className="website-rights">Counsly © 2021</small>
+          <div className="social-icons">
             <Link
-              className='social-icon-link'
-              to='/'
-              target='_blank'
-              aria-label='Facebook'
+              className="social-icon-link"
+              to="/"
+              target="_blank"
+              aria-label="Facebook"
             >
               <FaFacebook />
             </Link>
             <Link
-              className='social-icon-link'
-              to='/'
-              target='_blank'
-              aria-label='Instagram'
+              className="social-icon-link"
+              to="/"
+              target="_blank"
+              aria-label="Instagram"
             >
               <FaInstagram />
             </Link>
             <Link
-              className='social-icon-link'
+              className="social-icon-link"
               to={
-                '//www.youtube.com/channel/UCsKsymTY_4BYR-wytLjex7A?view_as=subscriber'
+                "//www.youtube.com/channel/UCsKsymTY_4BYR-wytLjex7A?view_as=subscriber"
               }
-              target='_blank'
-              aria-label='Youtube'
+              target="_blank"
+              aria-label="Youtube"
             >
               <FaYoutube />
             </Link>
             <Link
-              className='social-icon-link'
-              to='/'
-              target='_blank'
-              aria-label='Twitter'
+              className="social-icon-link"
+              to="/"
+              target="_blank"
+              aria-label="Twitter"
             >
               <FaTwitter />
             </Link>
             <Link
-              className='social-icon-link'
-              to='/'
-              target='_blank'
-              aria-label='LinkedIn'
+              className="social-icon-link"
+              to="/"
+              target="_blank"
+              aria-label="LinkedIn"
             >
               <FaLinkedin />
             </Link>
